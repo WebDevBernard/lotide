76fead6fb87829797ce66b6030649b7c396909e3
Bernard Yang <ybernard@gmail.com>
2021-05-27 01:51:38 +0000

    countOnly exercise

--- middle.js
+++ middle.js
@@ -18,24 +18,23 @@ const eqArrays = function (arrOne, arrTwo) {
   return true;
 }
 
-const middle = function(array){
-  newArr = []
-  if (array.length < 3) {
-    return newArr;
+assertArrayEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertArrayEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
+
+assertArrayEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertArrayEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
+
+
+const without = function (arrOne, arrTwo) {
+  let newArr = []
+  for (let i = 0; i < arrOne.length; i++){
+    if (arrOne[i] !== arrTwo[i]){
+      newArr.push(arrOne[i])
+    }
   }
-  if (array.length % 2 === 0){
-    newArr.push(array[array.length/2 - 1], array[array.length/2])
-      } else {
-    newArr.push(array[(array.length - 1) / 2]) //alternative way to do it is math.floor(array.length / 2)
-      }
   return newArr
-}
+} 
 
-assertArrayEqual(eqArrays(middle([1]), []), true) // => []
-assertArrayEqual(eqArrays(middle([1, 2]), []), true) // => []
-
-assertArrayEqual(eqArrays(middle([1, 2, 3]), [2]), true) // => [2]
-assertArrayEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true)// => [3]
-
-assertArrayEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true) // => [2, 3]
-assertArrayEqual(eqArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]), true)// => [3, 4]
+console.log(without([1, 2, 3], [1, 2]))
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
