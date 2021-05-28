18b8ba233c9ed4abfd307cd34f4e31df4d8bdfab
Bernard Yang <ybernard@gmail.com>
2021-05-28 01:31:15 +0000

    return a slice of an array until condition = truthy

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrOne.length; i++) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const takeUntil = function (array, callback) {
+  const sliceArray = [];
+  for (const x of array) {  // x of array is data1[i], array is data1
+    if (callback(x)) {   //callback is x >= x < 0 callback returns empty array
+      return sliceArray;  //returns empty array
+    }
+    sliceArray.push(index);
+  }
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual((results1), [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual((results2), ['I\'ve', 'been', 'to', 'Hollywood']);
