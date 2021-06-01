22e27dc93527e41b89e091329fa4a9ef3a9ca91b
Bernard Yang <ybernard@gmail.com>
2021-06-01 01:42:03 +0000

    added middleTest and tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,1 +1,11 @@
 const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns 2, 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns 2, 3, 4 for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+});
